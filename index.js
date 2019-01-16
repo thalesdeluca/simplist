9e00ebc5991bf3cdc43e18b0385badd338106ffe
Thales de Luca <delucathales@gmail.com>
2019-01-15 22:31:47 -0200

    Deploy error fix

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ if(process.env.NODE_ENV === "production"){
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
+  });
+}
 
 app.listen(process.env.PORT || 5000);
 
