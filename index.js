99da82dd52530c49e7128c6aff90f97cbe48415d
Thales de Luca <delucathales@gmail.com>
2019-01-15 22:24:02 -0200

    Deploy client side configuration

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ require('./routes/userAuth')(app);
 
 require('./routes/todo')(app);
 
+if(process.env.NODE_ENV === "production"){
+  //heroku deploy
+  app.use(express.static("/client/build"))
+
+  const path = require('path');
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
 
 app.listen(process.env.PORT || 5000);
 
