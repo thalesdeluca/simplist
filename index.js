9ccc4b566d2d9173f7d1c9af27ea374681d01542
Thales de Luca <delucathales@gmail.com>
2019-01-15 23:50:44 -0200

    fix

--- index.js
+++ index.js
@@ -18,14 +18,16 @@ require('./routes/userAuth')(app);
 
 require('./routes/todo')(app);
 
-if(process.env.NODE_ENV === "production"){
-  //heroku deploy
-  app.use(express.static("/client/build/"))
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
 
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
   const path = require('path');
-
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
